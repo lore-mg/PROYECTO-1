7f60f1b9b27504ac1178a151c6d31128e5cc7863
lore-mg <lorena.mg72@gmail.com>
2025-10-04 07:47:13 -0300

    Actualizado: pto 1 a 3 y 5

--- CodeChadsAcademy.java
+++ CodeChadsAcademy.java
@@ -4,13 +4,14 @@ public class CodeChadsAcademy {
         int[] puntajes = new int[5];  
         puntajes[0] = 75; //nota 1
         puntajes[1] = 85; //nota 2
-        puntajes[2] = 85; //nota 3
+        puntajes[2] = 65; //nota 3
         puntajes[3] = puntajes[1]<60 ? 100 : puntajes[1]; //nota 4
         int suma = puntajes[0]+puntajes[2];
         puntajes[4] = suma > 150 ? 95 : 70; //nota 5
         System.out.println("Notas: "+puntajes[0]+" "+puntajes[1]+" "+puntajes[2]+" "+puntajes[3]+" "+puntajes[4]);
 
-        //2da PARTE. Pto1
+        //2da PARTE. 
+        //Pto1
         int aprobadas = 0;
         for (int i=0; i<puntajes.length ; i++ ) {
             if(puntajes[i]>=60){
@@ -26,9 +27,19 @@ public class CodeChadsAcademy {
             System.out.println("Resultado: Algunas aprobadas. Sos un refactor en progreso.");
         }
 
-        //2da PARTE. Pto2
-
-        //2da PARTE. Pto3
+        //Pto2
+        int mayorVariacion = 0;
+        int posicion = 0;
+        for (int i = 0; i < puntajes.length - 1; i++) {
+            int diferencia = Math.abs(puntajes[i + 1] - puntajes[i]);
+            if (diferencia > mayorVariacion) {
+                mayorVariacion = diferencia;
+                posicion = i;
+            }
+        }
+        System.out.println("Mayor salto fue de "+mayorVariacion+" puntos entre la prueba "+(posicion+1)+" y la prueba "+(posicion+2));
+/* 
+        //Pto3
         int mejorNota = puntajes[0];
         for (int i=1; i<puntajes.length ; i++ ){
             if (mejorNota<=puntajes[i]){
@@ -40,8 +51,20 @@ public class CodeChadsAcademy {
                 System.out.println("¡Nivel PROGRESIVO! Sos un Stone Chad en crecimiento 📈");
             }
         }
-    
         
+        //Pto4
+
+        //Pto5 
+        int total = puntajes[0]+puntajes[1]+puntajes[2]+puntajes[3]+puntajes[4];
+        if (total<250){
+            System.out.println( "Normie total 😢");
+        } else if (total>=250 && total<=349) {
+            System.out.println("Soft Chad");
+        } else if (total>=350 && total<=449) {
+            System.out.println("Chad");
+        } else if (total>=450) {
+            System.out.println("Stone Chad definitivo 💪");
+        }*/
     } 
         
 }
